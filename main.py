02447aca10914a57afd25c5286a0bc2d0051914b
DariaZaytseva109 <dawazai@mail.ru>
2023-09-08 07:21:19 +0200

    изменила хранение - ранее слова хранились в словаре в программе. Теперь база SQLite (добавила в проект ОРМ SQLAlchemy

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-
 import telebot
 from telebot import types
 from telebot.types import InlineKeyboardButton
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from storage import engine, User
 
-from model import Db, Word_to_remember
 
-API_TOKEN = '6474871614:AAESLEJ4U7wQQ9l9DA0r7s-wkuuzF4v1CEc'
+
+API_TOKEN = ''
 bot = telebot.TeleBot(API_TOKEN)
-db = Db()
+
 
 
 @bot.message_handler()
@@ -15,7 +17,7 @@ def send_welcome(message):
     bot.send_message(chat_id=message.chat.id, text='Что делаем?')
     markup = types.InlineKeyboardMarkup()
     button_1 = InlineKeyboardButton('Ввести новое слово', callback_data='input_new_word')
-    button_2 = InlineKeyboardButton('Проверить знания', callback_data='test')
+    button_2 = InlineKeyboardButton('Повторить слова', callback_data='test')
     markup.row(button_1, button_2)
     bot.send_message(message.chat.id, text='Выбери:', reply_markup=markup)
 
@@ -30,34 +32,31 @@ def input_new_word(callback_obj):
 
 @bot.message_handler(content_types=['text'])
 def set_into_dict(message):
-    user_id = message.from_user.id
     user_msg = message.text
-    new_word = Word_to_remember(text=user_msg, meaning=None)
-    lst = db.dct.get(user_id, [])
-    lst.append(new_word)
-    db.dct[user_id] = lst
-    print(db.dct)
     bot.reply_to(message, 'Введите значение слова')
-    bot.register_next_step_handler(message, set_meaning)
+    bot.register_next_step_handler(message, set_meaning, word=user_msg)
 
 
 @bot.message_handler(content_types=['text'])
-def set_meaning(message):
+def set_meaning(message, word):
+    word = word
     meaning = message.text
     user_id = message.from_user.id
-    db.dct[user_id][-1].meaning = meaning
-    print(db.dct)
-    bot.reply_to(message, f'Добавлено слово:{db.dct[user_id][-1].text} со значением {meaning}')
+    session = Session(engine)
+    new_word = User(word=word, user_id=user_id, meaning=meaning)
+    session.add(new_word)
+    session.commit()
+    bot.reply_to(message, f'Добавлено слово:  {new_word.word}   со значением   {meaning} ')
 
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'test')
 def test(callback_obj):
-    print(db.dct[callback_obj.from_user.id])
-    whole_dict = [f'{x.text} - {x.meaning}' for x in (db.dct[callback_obj.from_user.id])]
-
-    for w in whole_dict:
-        bot.send_message(callback_obj.from_user.id, w)
+    session = Session(engine)
+    result = select(User).where(User.user_id == callback_obj.from_user.id)
+    print(callback_obj.from_user.id)
+    for word in session.scalars(result):
+        bot.send_message(callback_obj.from_user.id, f' {word.word}  -  {word.meaning}')
 
 
 
--- model.py
+++ model.py
@@ -1,21 +1,13 @@
 from telebot.types import User
 
 
-class Db:
-    def __init__(self):
-        self.dct = {}
-
-
 class My_user(User):
     def __init__(self, is_bot, first_name, user_dict):
         self.user_dict = user_dict
         super().__init__(id, is_bot, first_name)
 
 
-class Word_to_remember:
-    def __init__(self, text, meaning, is_remembered=False):
-        self.text = text
-        self.meaning = meaning
-        self.is_remembered = is_remembered
+
+
 
 
